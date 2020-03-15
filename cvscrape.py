07814f5c35390b92711a4144c1d13d744daac3bb
Pycro <pycrocoding@gmail.com>
2020-03-15 14:35:12 +0000

    first commit

---
+++ cvscrape.py
@@ -0,0 +1,62 @@
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+from datetime import datetime
+
+#next two lines raspberry pi only and sensehat
+from sense_hat import SenseHat
+sense = SenseHat()
+
+#create logfile
+f = open("cvlog.txt", "a")
+f.write("Date,Time,Cases,Deaths,Recovered\n")
+f.close()
+
+while True:
+    #check if on the hour for update
+    currentdate = datetime.now().strftime('%d-%m-%Y')
+    currenttime = datetime.now().strftime('%H:%M')
+    hrcheck = currenttime.split(":")
+    hrcheck2 = hrcheck[1]
+    #print(hrcheck2)
+    print(currentdate)
+    print(currenttime)
+    results = "0,0,0,0"
+    if(hrcheck2.startswith("00")):
+        #grab fresh data
+        URL = 'https://www.worldometers.info/coronavirus/country/uk/'
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        cases = soup.find_all('div', class_='maincounter-number')
+        results = ""
+        for span in cases:
+            elem = span.find('span',)
+            results += elem.text.replace(",","") + ","
+        results2 = results.split(",")
+        cases = results2[0].strip()
+        deaths = results2[1].strip()
+        recovered = results2[2].strip()
+
+        
+        f = open("cvlog.txt", "a")
+        f.write(currentdate + "," + currenttime + "," + cases + "," + deaths + "," + recovered+"\n")
+        f.close()
+            
+    #print(results)
+    results = results.split(",")
+    cases = results[0].strip()
+    deaths = results[1].strip()
+    recovered = results[2].strip()
+    print("Cases = " + cases)
+    print("Deaths = " + deaths)
+    print("Recovered = " + recovered)
+    message = "Cases = " + cases + " Deaths = " + deaths + " Rec = " + recovered
+    print(message)
+
+    sense.show_message(message,text_colour=[255,0,0],back_colour=[0,255,0])
+    sense.show_message('')
+
+
+    sleep(60)
+
+
